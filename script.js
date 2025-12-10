3982838ca5d58b19d32c0e9e39e1d9ebc991b89f
bibek935 <bibekranabhat414@gmail.com>
2025-12-10 11:59:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function showScreen(title, description, imageFile) {
     showScreen(
       "画面1",
       "ここは画面1です。",
-      "screen1.png"
+      "images"
     );
   });
 }
@@ -25,7 +25,7 @@ document.getElementById("btn1").addEventListener("click", () => {
   showScreen(
     "画面1",
     "ここは画面1です。",
-    "screen1.png"
+    "images.png"
   );
 });
 
@@ -34,7 +34,7 @@ document.getElementById("btn2").addEventListener("click", () => {
   showScreen(
     "画面2",
     "ここは画面2です。",
-    "screen2.png"
+    "images(1)"
   );
 });
 
@@ -43,7 +43,7 @@ document.getElementById("btn3").addEventListener("click", () => {
   showScreen(
     "画面3",
     "ここは画面3です。",
-    "screen3.png"
+    "screen(2).png"
   );
 });
 
